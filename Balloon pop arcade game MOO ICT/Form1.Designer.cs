6ac11c20cfdf11ee96582c293ad254891301baec
Anhar Ali <67914277+mooict@users.noreply.github.com>
2021-01-03 14:56:52 +0000

    Add files via upload

---
+++ Balloon pop arcade game MOO ICT/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Balloon_pop_arcade_game_MOO_ICT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.bomb = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bomb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Balloon_pop_arcade_game_MOO_ICT.Properties.Resources.pinkBalloon;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 173);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 143);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "balloon";
+            this.pictureBox1.Click += new System.EventHandler(this.PopBalloon);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Balloon_pop_arcade_game_MOO_ICT.Properties.Resources.redHeart;
+            this.pictureBox2.Location = new System.Drawing.Point(182, 73);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(111, 116);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "balloon";
+            this.pictureBox2.Click += new System.EventHandler(this.PopBalloon);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Balloon_pop_arcade_game_MOO_ICT.Properties.Resources.redStar;
+            this.pictureBox3.Location = new System.Drawing.Point(347, 141);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(111, 116);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "balloon";
+            this.pictureBox3.Click += new System.EventHandler(this.PopBalloon);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Balloon_pop_arcade_game_MOO_ICT.Properties.Resources.yellowBalloon;
+            this.pictureBox4.Location = new System.Drawing.Point(358, 328);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(111, 116);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "balloon";
+            this.pictureBox4.Click += new System.EventHandler(this.PopBalloon);
+            // 
+            // bomb
+            // 
+            this.bomb.Image = global::Balloon_pop_arcade_game_MOO_ICT.Properties.Resources.bomb;
+            this.bomb.Location = new System.Drawing.Point(150, 423);
+            this.bomb.Name = "bomb";
+            this.bomb.Size = new System.Drawing.Size(100, 98);
+            this.bomb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.bomb.TabIndex = 0;
+            this.bomb.TabStop = false;
+            this.bomb.Click += new System.EventHandler(this.GoBoom);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(13, 13);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(88, 24);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(634, 661);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.bomb);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Balloon Pop Arcade Game MOO ICT";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bomb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox bomb;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Balloon pop arcade game MOO ICT/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Balloon_pop_arcade_game_MOO_ICT
+{
+    public partial class Form1 : Form
+    {
+
+        int speed;
+        int score;
+        Random rand = new Random();
+        bool gameOver;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+
+            txtScore.Text = "Score: " + score;
+
+            if (gameOver == true)
+            {
+                gameTimer.Stop();
+                txtScore.Text = "Score: " + score + " Game over, press enter to restart!";
+            }
+
+            foreach(Control x in this.Controls)
+            {
+
+                if (x is PictureBox)
+                {
+                    x.Top -= speed;
+
+                    if (x.Top < -100)
+                    {
+                        x.Top = rand.Next(700, 1000);
+                        x.Left = rand.Next(5, 500);
+                    }
+
+                    if ((string)x.Tag == "balloon")
+                    {
+
+                        if (x.Top < -50)
+                        {
+                            gameOver = true;
+                        }
+
+                        if (bomb.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            x.Top = rand.Next(700, 1000);
+                            x.Left = rand.Next(5, 500);
+                        }
+                    }
+                }
+
+            }
+
+
+            if (score > 5)
+            {
+                speed = 8;
+            }
+
+            if (score > 15 && score < 25)
+            {
+                speed = 12;
+            }
+
+
+
+        }
+
+        private void PopBalloon(object sender, EventArgs e)
+        {
+            if (gameOver == false)
+            {
+                var balloon = (PictureBox)sender;
+
+                balloon.Top = rand.Next(750, 1000);
+                balloon.Left = rand.Next(5, 500);
+
+                score += 1;
+
+            }
+
+
+        }
+
+        private void GoBoom(object sender, EventArgs e)
+        {
+            if (gameOver == false)
+            {
+                bomb.Image = Properties.Resources.boom;
+                gameOver = true;
+            }
+
+
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && gameOver == true)
+            {
+                RestartGame();
+            }
+        }
+
+        private void RestartGame()
+        {
+            speed = 5;
+            score = 0;
+            gameOver = false;
+
+            bomb.Image = Properties.Resources.bomb;
+
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    x.Top = rand.Next(750,  1000);
+                    x.Left = rand.Next(5, 500);
+                }
+            }
+
+            gameTimer.Start();
+        }
+    }
+}
